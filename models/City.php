2c32d084ed721a9b82cc655b0b2734c424add31b
Surahman <surahman.duang@gmail.com>
2016-06-27 14:00:26 +0700

    create city model and implement city model behavior

---
+++ models/City.php
@@ -0,0 +1,38 @@
+<?php namespace Octommerce\Shipping\Models;
+
+use Model;
+
+/**
+ * City Model
+ */
+class City extends Model
+{
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'octommerce_shipping_cities';
+
+    /**
+     * Softly implement the CityModel behavior.
+     **/
+    public $implement = ['@Octommerce.Octommerce.Behaviors.CityModel'];
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $belongsTo = [
+        'state' => 'RainLab\Location\Models\State',
+    ];
+
+}
